07efdfef65f4efc7c0403e7c2ba97a4fa2d16b58
Cameron King <cameron@zerolag.com>
2013-10-18 01:43:08 +0000

    add header and footer to docs

--- bin/makedocs.sh
+++ bin/makedocs.sh
@@ -5,3 +5,10 @@ find . -type d -exec mkdir -p ../doc/{} \;
 find . -type f -name "*.litcoffee" -exec markdown_py -f ../doc/{}.html {} \;
 find . -type f -name "*.md" -exec markdown_py -f ../doc/{}.html {} \;
 
+cd ../doc || exit 1
+for f in `find * -type f -name "*.html"`; do
+  cp $f $f.tmp
+  cat .head.html $f.tmp .foot.html >$f
+  rm $f.tmp
+done
+
